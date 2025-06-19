12563eb0d305e2884d4f0d3bf6eec7f9ad76f0a7
DexT_17 Teerth <teerth.kulkarni@walchandsangli.ac.in>
2025-06-20 01:53:58 +0530

    working tailwind

--- frontend/FE/src/App.jsx
+++ frontend/FE/src/App.jsx
@@ -1,29 +1,30 @@
+// App.js
+
 import './App.css';
-import {Routes, Route, BrowserRouter , useNavigate} from "react-router-dom"
+import { Routes, Route, BrowserRouter, useNavigate } from "react-router-dom";
 import Login from '../component/Login';
 
 function Home() {
-  const nav = useNavigate()
+  const nav = useNavigate();
   return (
-    <>
-      <h1>home page</h1>
-       <button onClick={()=>{nav("/login")}}>Login</button>
-    </>
+    <div className='landingPage w-screen'>
+      <div className='navBar flex justify-around w-screen bg-red-500 text-white p-4'>
+        <button>Sign up</button>
+        <button>Login</button>
+      </div>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
-
-function App(){
-  
-  return(
-    <>
-      <BrowserRouter>
-          <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/login' element={<Login/>}/>
-          </Routes>
-      </BrowserRouter>  
-    </>
-  )
-}
